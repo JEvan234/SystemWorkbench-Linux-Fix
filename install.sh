75ddb30bf4023ffe11ee0edb528f7efab261f0a4
Jacob Evans <Jevan234@students.kennesaw.edu>
2026-02-23 15:49:33 -0500

    added early flatpak checks

--- install.sh
+++ install.sh
@@ -2,4 +2,8 @@
 
 #Signify beginning of software and check for pre-reqs
 echo "Starting Install"
+
+#Check for flatpak and bottles
 echo "Checking for Bottles(Flatpak)"
+flatpak --version
+flatpak list | grep "bottles"
